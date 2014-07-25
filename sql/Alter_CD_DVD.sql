7c3184937cbd1437ef87937a35d2fbc489084a4b
Michael Eaton <mjeaton@gmail.com>
2014-07-25 13:16:34 -0400

    added script to add additional columns.

---
+++ sql/Alter_CD_DVD.sql
@@ -0,0 +1,18 @@
+alter table CD
+	add Genre varchar(50) not null
+
+alter table CD
+	add [Year] int not null
+
+alter table CD
+	add Title varchar(50) not null
+
+
+alter table DVD
+	add Genre varchar(50) not null
+
+alter table DVD
+	add [Year] int not null
+
+alter table DVD
+	add Title varchar(50) not null
