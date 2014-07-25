9abf6c276745cfb56c6b49024190df0cc104c522
Michael Eaton <mjeaton@gmail.com>
2014-07-25 11:20:15 -0400

    added web project

---
+++ src/MediaManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+
+namespace MediaManager.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var m = new CombinedModel();
+            m.Person = new MyModel();
+            m.Person.FirstName = "Mike";
+            m.Person.LastName = "Eaton";
+
+            m.Random = new AnotherModel();
+            m.Random.SomePieceOfData = "This is a random string";
+
+            ViewBag.Message = "Your application description page.";
+            ViewBag.SubTitle = "This is the title";
+
+            return View(m);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class MyModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class AnotherModel
+    {
+        public string SomePieceOfData { get; set; }
+    }
+
+    public class CombinedModel
+    {
+        public MyModel Person { get; set; }
+        public AnotherModel Random { get; set; }
+    }
+}
